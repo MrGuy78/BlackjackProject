81d4290083544ffe9e34bb731597bd70e4b14486
Andrew Nelson <nels4766@gmail.com>
2024-11-15 16:52:05 -0700

    Basic Setup in class

---
+++ src/com/skilldistillery/cards/blackjack/BlackjackHand.java
@@ -0,0 +1,18 @@
+package com.skilldistillery.cards.blackjack;
+
+import com.skilldistillery.cards.common.AbstractHand;
+
+public class BlackjackHand extends AbstractHand {
+
+	//NO fields
+	
+	
+	
+	@Override
+	public int getHandValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
---
+++ src/com/skilldistillery/cards/blackjack/Player.java
@@ -0,0 +1,20 @@
+package com.skilldistillery.cards.blackjack;
+
+import com.skilldistillery.cards.common.Card;
+
+public class Player {
+	
+	//FIELDS
+	private BlackjackHand hand;
+	//NO List<Card>
+	
+	public Player() {
+		hand = new BlackjackHand();
+	}
+	
+	public void hitMe(Card card) {
+		hand.addCard(card);
+	}
+	
+	//NO getHand()
+}
